bc1a0e9cc2833148b69cecae811f6ab1aae7077c
Yulchikss <yuliapelia19@gmail.com>
2024-06-29 22:03:14 +0300

    feat: displayed meals

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+import MealItem from '../components/MealItem';
+
 import { MEALS } from '../data/dummy-data';
 
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({route}) => {
 
+  const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((meal) => meal.categoryIds.indexOf(categoryId) >= 0);
+
+    const renderMealItem = (itemData) => {
+        return <MealItem title={itemData.item.title} />
+        
+    }   
     return (
         <View styles={styles.container} >
-            <Text>
-                Meals Overview Screen
-            </Text>
+            <FlatList data={displayedMeals} keyExtractor={(meal) => meal.id} renderItem={renderMealItem}></FlatList>
         </View>
     )
 
